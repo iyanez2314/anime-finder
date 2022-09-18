395900b83fe105e5ffd52f59737fa51686716958
Isaac <isaac231467@gmail.com>
2022-09-18 12:26:56 -0500

    was able to get the users input so the api knows what to look for, I also was able to display the information about the anime

--- js/inex.js
+++ js/inex.js
@@ -1,22 +1,61 @@
-const submitElement = document.getElementById('currentAnimeButton');
 
-const buttonClick = (event) =>{
-    event.preventDefault(); // Stops the page from refreshing
-    console.log(event);
-}
+// all the elements that are on the dom
+const submitElement = document.getElementById('currentAnimeButton');
+const infoContainer = document.getElementById('info-container');
+
+// all the elements we are creating
+const currentAnimeTitle = document.createElement('h1');
+const currentAnimeBackground = document.createElement('p')
+const currentAnimeScore = document.createElement('p')
+const currentAnimeStatus = document.createElement('p')
+const currentAnimeEpisodes = document.createElement('p')
+const currentAnimeDuration = document.createElement('p')
+
+// Api we are using
+let api = 'https://api.jikan.moe/v4/anime?q='
 
 const currentAnimeInputValue = () => {
     let currentAnimeInput = document.getElementById('currentAnimeInput');
-    console.log(currentAnimeInput.value)
+    let currentAnimeTitle = currentAnimeInput.value; 
+    fetch(api + currentAnimeTitle)
+    .then((response) => response.json())
+    .then((data) => infoHandler(data))
+};
+
+
+
+// I want to create another function that will basically get the data from the first function in the second promise chain.
+const infoHandler = (data) => {
+    const currentDataTitle = data.data[0].title
+    const currentDataBackground = data.data[0].background
+    const currentDataScore = data.data[0].score
+    const currentDataStatus = data.data[0].status
+    const currentDataEpisodes = data.data[0].episodes
+    const currentDataDuration = data.data[0].duration
+
+
+    currentAnimeTitle.textContent = currentDataTitle;
+    currentAnimeBackground.textContent = currentDataBackground;
+    currentAnimeScore.textContent = currentDataScore;
+    currentAnimeStatus.textContent = currentDataStatus;
+    currentAnimeEpisodes.textContent = currentDataEpisodes;
+    currentAnimeDuration.textContent = currentDataDuration
+    
+   
+
+    infoContainer.appendChild(currentAnimeTitle);
+    infoContainer.appendChild(currentAnimeBackground);
+    infoContainer.appendChild(currentAnimeScore);
+    infoContainer.appendChild(currentAnimeStatus);
+    infoContainer.appendChild(currentAnimeEpisodes);
+    infoContainer.appendChild(currentAnimeDuration);
 }
 
-submitElement.addEventListener('click', buttonClick)
+submitElement.addEventListener('click', currentAnimeInputValue);
 
 
+ // infoContainerTitlesLi.textContent = currentAnime.title;
+ // infoTitleList.appendChild(infoContainerTitlesLi);
 
-// const api = 'https://api.jikan.moe/v4/anime?q='
-// const currentAnimeSelected = 'bleach'
-
-// fetch(api + currentAnimeSelected )
-// .then((response) => response.json())
-// .then((data) => console.log(data))
+  // infoContainerTitle.textContent = currentAnime;
+    // infoContainer.appendChild(infoContainerTitle);
