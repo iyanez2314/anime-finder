7b1b624199fedbf43855d57774e42fa3227928f0
Isaac <isaac231467@gmail.com>
2022-09-19 12:17:11 -0500

    was able to add some text when the data comes back being null, I am going to work on the next todo

--- js/inex.js
+++ js/inex.js
@@ -25,10 +25,13 @@ const currentAnimeInputValue = () => {
 };
 
 /*
-    TODO: I need to do some conditional checking for items that are null.
     TODO: I need to figure out how to add pagination when the response has more than 1 page.
+        - I will need to make variable that will hold the page we are on
+        - I will need to make functions that will handle all of this movement going on
+        - I probably need to check in data.pagination
     TODO: How I can add a modal so when a user clicks the anime it'll expand for more information.
     TODO: Figure how I can add a watchlist so the user can choose animes to add to their wishlist. 
+    TODO: Figure out how to refresh the page when we input another anime if we already seached for an anime.
 */
 
 
@@ -39,6 +42,14 @@ const infoHandler = (data) => {
     const testData = data.data;
 
     testData.forEach(anime => {
+
+        let animeTitle = anime.title;
+        let animeBackground = anime.background;
+        let animeScore = anime.score;
+        let animeStatus = anime.status;
+        let animeEpisodes = anime.episodes;
+        let animeDuration = anime.duration;
+        
         const animeElementTitle = document.createElement('p');
         const animeElementBackground = document.createElement('p');
         const animeElementScore = document.createElement('p');
@@ -46,12 +57,12 @@ const infoHandler = (data) => {
         const animeElementEpisodes = document.createElement('p');
         const animeElementDuration = document.createElement('p');
 
-        animeElementTitle.innerText = `Names: ${anime.title}`;
-        animeElementBackground.innerHTML = `Background of anime: ${anime.background}`;
-        animeElementScore.innerHTML = `Anime Score: ${anime.score}`;
-        animeElementStatus.innerHTML = `Anime status: ${anime.status}`;
-        animeElementEpisodes.innerHTML = `Anime Episodes: ${anime.episodes}`;
-        animeElementDuration.innerHTML = `Anime duration: ${anime.duration}`;
+        animeElementTitle.innerText = `Names: ${animeTitle ? animeTitle : 'No data'}`;
+        animeElementBackground.innerHTML = `Background of anime: ${animeBackground ? animeBackground : 'No background information at this time'}`;
+        animeElementScore.innerHTML = `Anime Score: ${animeScore ? animeScore : 'No score at this time'}`;
+        animeElementStatus.innerHTML = `Anime status: ${animeStatus ? animeStatus : 'No status at this time'}`;
+        animeElementEpisodes.innerHTML = `Anime Episodes: ${animeEpisodes ? animeEpisodes : 'No information on episodes at this time'}`;
+        animeElementDuration.innerHTML = `Anime duration: ${animeDuration ? animeDuration : 'No information on the duration at this time'}`;
 
         infoContainer.append(animeElementTitle);
         infoContainer.append(animeElementBackground);
@@ -60,7 +71,6 @@ const infoHandler = (data) => {
         infoContainer.append(animeElementEpisodes);
         infoContainer.append(animeElementDuration);
     })
-   
 };
 
 // here we are adding an event listener to the button and that event is a click event and then we are specifying what function we are going to preform once that button in clicked.
